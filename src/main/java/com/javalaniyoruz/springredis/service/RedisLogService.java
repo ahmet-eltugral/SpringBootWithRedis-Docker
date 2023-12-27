d0161e5b63b5fb160195c3312cc7dd425a28a7c6
Ahmet <ahmet.vbt.com.tr>
2023-12-27 17:53:43 +0300

    Docker Log

---
+++ src/main/java/com/javalaniyoruz/springredis/service/RedisLogService.java
@@ -0,0 +1,2 @@
+package com.javalaniyoruz.springredis.service;public interface RedisLogService {
+}
---
+++ src/main/java/com/javalaniyoruz/springredis/serviceImpl/RedisLogServiceImpl.java
@@ -0,0 +1,2 @@
+package com.javalaniyoruz.springredis.serviceImpl;public class RedisLogServiceImpl {
+}
